cc3a516050d9f01b02a69321a07bd880fc7e59c4
ArefBitar <arefbitar@gmail.com>
2022-04-01 17:45:14 +0200

    Step 7 code api

--- server/api.js
+++ server/api.js
@@ -1,11 +1,16 @@
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
-
 const PORT = 8092;
-
 const app = express();
+var database = express();
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require("mongodb").ObjectID;
+
+const URI = "mongodb+srv://arefbitar:admin@cluster0.8zdw6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const  DATABASE_NAME = "clear-fashion";
+const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 module.exports = app;
 
 app.use(require('body-parser').json());
@@ -18,6 +23,10 @@ app.get('/', (request, response) => {
   response.send({'ack': true});
 });
 
+
+
 app.listen(PORT);
+
+
 
 console.log(`📡 Running on port ${PORT}`);
